f452131c9b62f897a2a6d411daabffa5692013f9
nermineayadi <nermine.ayadi15@gmail.com>
2019-03-12 23:12:45 +0100

    v0.1
    v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import {MatToolbarModule} from '@angular/material/toolbar';
-=======
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> 1b71fc858c313b56cf357d899b0f2d162bc9bdf5
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThemeComponent } from './theme/theme.component';
@@ -15,24 +12,18 @@ import { BarmanComponent } from './pages/barman/barman.component';
 import { ServeurComponent } from './pages/serveur/serveur.component';
 import { PortionneurComponent } from './pages/portionneur/portionneur.component';
 import { RespcomptoireComponent } from './pages/respcomptoire/respcomptoire.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CuisinierComponent } from './pages/cuisinier/cuisinier.component';
 import { GrowlModule } from 'primeng/primeng';
-import {MatIconModule} from '@angular/material/icon';
 import {AccordionModule} from 'primeng/accordion';   
-import {MatCheckboxModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, } from '@angular/material';
 import {SidebarModule} from 'primeng/sidebar';
 import {MatButtonModule} from '@angular/material/button';
 import { NavigationComponent } from './navigation/navigation.component';
 import { NavbarComponent } from './navbar/navbar.component';
-=======
 import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbSidebarService } from '@nebular/theme';
-import { CuisinierComponent } from './pages/cuisinier/cuisinier.component';
 import { LoginComponent } from './auth/login/login.component';
 import {MatangModule  } from "../assets/matang.module";
 
->>>>>>> 1b71fc858c313b56cf357d899b0f2d162bc9bdf5
 @NgModule({
   declarations: [
     AppComponent,
@@ -44,34 +35,28 @@ import {MatangModule  } from "../assets/matang.module";
     BarmanComponent,
     ServeurComponent,
     PortionneurComponent,
-<<<<<<< HEAD
     RespcomptoireComponent,
     NavigationComponent,
-    NavbarComponent
+    NavbarComponent,
     
     
-=======
-    RespcomptoireComponent,LoginComponent, 
->>>>>>> 1b71fc858c313b56cf357d899b0f2d162bc9bdf5
+    RespcomptoireComponent,LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     BrowserAnimationsModule,
     MatToolbarModule,
     GrowlModule,
     AccordionModule,
     SidebarModule,MatButtonModule,
     
-    MatCheckboxModule,MatInputModule,MatProgressSpinnerModule,MatCardModule,MatMenuModule, MatIconModule
-=======
+    
     MatangModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
     NbSidebarModule,    
     HttpClientModule, 
->>>>>>> 1b71fc858c313b56cf357d899b0f2d162bc9bdf5
   ],
   providers: [],
   bootstrap: [AppComponent]
