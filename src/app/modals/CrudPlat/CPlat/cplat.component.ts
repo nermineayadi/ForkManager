89f3d750d6f261634a9855ae6e28ba718a6f53c0
Meriem <meriem-chaieb@live.fr>
2019-04-04 02:34:53 +0100

    plat sauf image

--- src/app/modals/CrudPlat/CPlat/cplat.component.ts
+++ src/app/modals/CrudPlat/CPlat/cplat.component.ts
@@ -1,53 +1,92 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, } from '@angular/material';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export interface Ingredients {
   position: number;
-  ingredient: string;
+  nom: string;
   quantite: number;
   unite: string;
 }
-const ELEMENT_DATA: any[] = [
-  {position: 1, ingredient: 'Hydrogen', quantite: 1.0079, unite: 'H'},
-  {position: 2, ingredient: 'Helium', quantite: 4.0026, unite: 'He'},
-  {position: 3, ingredient: 'Lithium', quantite: 6.941, unite: 'Li'},
-  {position: 4, ingredient: 'Beryllium', quantite: 9.0122, unite: 'Be'},
+export interface SousRecettes {
+  position: number;
+  nom: string;
+  quantite: number;
+  unite: string;
+}
+export interface Etapes {
+  position: number;
+  nom: string;
+  description: string;
+}
+const ingredients: Ingredients[] = [
+  { position: 1, nom: 'Hydrogen', quantite: 1.0079, unite: 'H' },
+  { position: 2, nom: 'Helium', quantite: 4.0026, unite: 'He' },
+  { position: 3, nom: 'Lithium', quantite: 6.941, unite: 'Li' },
+  { position: 4, nom: 'Beryllium', quantite: 9.0122, unite: 'Be' },
+
+];
+const sousRecettes: SousRecettes[] = [
+  { position: 1, nom: 'Hydrogen', quantite: 1.0079, unite: 'H' },
+  { position: 2, nom: 'Helium', quantite: 4.0026, unite: 'He' },
+  { position: 3, nom: 'Lithium', quantite: 6.941, unite: 'Li' },
+  { position: 4, nom: 'Beryllium', quantite: 9.0122, unite: 'Be' },
+
+];
+const etapes: Etapes[] = [
+  { position: 1, nom: 'Hydrogen', description: 'H' },
+  { position: 2, nom: 'Helium', description: 'He' },
+  { position: 3, nom: 'Lithium', description: 'Li' },
+  { position: 4, nom: 'Beryllium', description: 'Be' },
 
 ];
 @Component({
-    selector: 'app-cplat',
-    templateUrl: './cplat.component.html',
+  selector: 'app-cplat',
+  templateUrl: './cplat.component.html',
+  styleUrls: ['./cplat.component.scss'],
 })
-export class CplatComponent  implements OnInit{
-  displayedColumns: string[] = ['position', 'ingredient', 'quantité', 'unité'];
-  dataSource = ELEMENT_DATA;
-
-
-  firstFormGroup: FormGroup;
-  secondFormGroup: FormGroup;
-  thirdFormGroup: FormGroup;
-  constructor(private _formBuilder: FormBuilder, public dialogRef: MatDialogRef<CplatComponent>) {}
+export class CplatComponent implements OnInit {
+  photo : string;
+  unites:string[] = ['g','ml','portion']
+  //colonnes
+  ingredientColumns: string[] = ['position', 'nom', 'quantité', 'unité'];
+  sousRecetteColumns: string[] = ['position', 'nom', 'quantité', 'unité','voir'];
+  etapeColumns: string[] = ['position', 'nom', 'description'];
+  //tableaux dataSource
+  ingredient_tab = ingredients;
+  sousRecette_tab = sousRecettes;
+  etape_tab = etapes;
+  //formGroups
+  ingredient: FormGroup;
+  sousRecette: FormGroup;
+  etape: FormGroup;
+  //constructeur
+  constructor(private _formBuilder: FormBuilder, public dialogRef: MatDialogRef<CplatComponent>) { }
 
   ngOnInit() {
-    this.firstFormGroup = this._formBuilder.group({
-      ingredient: ['', Validators.required],
+    this.ingredient = this._formBuilder.group({
+      nom: ['', Validators.required],
       quantite: ['', Validators.required],
       unite: ['', Validators.required]
     });
-    this.secondFormGroup = this._formBuilder.group({
+    this.sousRecette = this._formBuilder.group({
+      nom: ['', Validators.required],
+      quantite: ['', Validators.required],
+      unite: ['', Validators.required]
     });
-    this.thirdFormGroup = this._formBuilder.group({
+    this.etape = this._formBuilder.group({
+      nom: ['', Validators.required],
+      description: ['', Validators.required],
     });
   }
-   
-    
-      onNoClick(): void {
-        this.dialogRef.close();
-      }
-    
+  onchange(evt){
+  const file = event.target.files[0]
+}
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
 
 
 
-  
-    
+
