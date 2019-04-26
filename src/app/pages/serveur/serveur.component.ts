c15d889bae45ce227735e153df05c0f006ac38c5
nermine <nermine.ayadi15@gmail.com>
2019-04-26 16:27:10 +0100

    Interface Serveur

--- src/app/pages/serveur/serveur.component.ts
+++ src/app/pages/serveur/serveur.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ShareService } from 'src/app/services/share.service';
+export interface Transaction {
+  item: string;
+  cost: number;
+}
 
 @Component({
   selector: 'app-serveur',
@@ -8,7 +12,18 @@ import { ShareService } from 'src/app/services/share.service';
   styleUrls: ['./serveur.component.scss']
 })
 export class ServeurComponent implements OnInit {
-
+  displayedColumns: string[] = ['item', 'cost'];
+  transactions: Transaction[] = [
+    {item: 'Beach ball', cost: 4},
+    {item: 'Towel', cost: 5},
+    {item: 'Frisbee', cost: 2},
+    {item: 'Sunscreen', cost: 4},
+    {item: 'Cooler', cost: 25},
+    {item: 'Swim suit', cost: 15},
+  ];
+  getTotalCost() {
+    return this.transactions.map(t => t.cost).reduce((acc, value) => acc + value, 0);
+  }
   page: number = 3;
     opened = false;
     onSerch = false;
